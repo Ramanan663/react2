67d1d47a2b2abca5df4f47c18d66cfdf9027ee24
ramanan <nnraam@hotmail.com>
2021-01-08 14:37:33 +0100

    react1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Container from "./Container";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="groceries-header"></header>
+      <Container />
+    </div>
+  );
+}
+
+export default App;
